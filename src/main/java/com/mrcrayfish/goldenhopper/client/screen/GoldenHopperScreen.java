c654d37dd882f3ccf2b711beb12b32659c5fbee2
Thorbjörn Sundragon <puggan@spiro.se>
2020-07-02 01:27:04 +0200

    Shorten the MCP-name comments, to give a better overview

--- src/main/java/com/mrcrayfish/goldenhopper/client/screen/GoldenHopperScreen.java
+++ src/main/java/com/mrcrayfish/goldenhopper/client/screen/GoldenHopperScreen.java
@@ -27,45 +27,48 @@ public class GoldenHopperScreen extends ContainerScreen<GoldenHopperContainer>
     }
 
     @Override
-    // TODO MCP-name: public void render(int mouseX, int mouseY, float partialTicks)
+    // TODO MCP-name: func_230430_a_ -> render
     public void func_230430_a_(MatrixStack p_230450_1_, int mouseX, int mouseY, float partialTicks)
     {
-        // TODO MCP-name: this.renderBackground();
+        // TODO MCP-name: func_230446_a_ -> renderBackground
         this.func_230446_a_(p_230450_1_);
-        // TODO MCP-name: super.render(mouseX, mouseY, partialTicks);
+        // TODO MCP-name: func_230430_a_ -> render
         super.func_230430_a_(p_230450_1_, mouseX, mouseY, partialTicks);
-        // TODO MCP-name: this.renderHoveredToolTip(mouseX, mouseY);
+        // TODO MCP-name: func_230459_a_ -> renderHoveredToolTip
         this.func_230459_a_(p_230450_1_, mouseX, mouseY);
     }
 
     @Override
-    // TODO MCP-name: protected void drawGuiContainerForegroundLayer(int mouseX, int mouseY)
+    // TODO MCP-name: func_230451_b_ -> drawGuiContainerForegroundLayer
     protected void func_230451_b_(MatrixStack p_230451_1_, int mouseX, int mouseY)
     {
-        // TODO MCP-name: this.font.drawString(this.title.getFormattedText(), 8.0F, 6.0F, 4210752);
+        // TODO MCP-name: field_230712_o_ -> font
+        // TODO MCP-name: func_238422_b_ -> drawString
+        // TODO MCP-name: field_230704_d_ -> title
         this.field_230712_o_.func_238422_b_(p_230451_1_, this.field_230704_d_, 8.0F, 6.0F, 4210752);
-        // TODO MCP-name: this.font.drawString(this.playerInventory.getDisplayName().getFormattedText(), 8.0F, (float)(this.ySize - 96 + 2), 4210752);
+        // TODO MCP-name: field_230712_o_ -> font
+        // TODO MCP-name: func_238422_b_ -> drawString
         this.field_230712_o_.func_238422_b_(p_230451_1_, this.playerInventory.getDisplayName(), 8.0F, (float)(this.ySize - 96 + 2), 4210752);
     }
 
     @Override
-    // TODO MCP-name: protected void drawGuiContainerBackgroundLayer(float partialTicks, int mouseX, int mouseY)
+    // TODO MCP-name: func_230450_a_ -> drawGuiContainerBackgroundLayer
     protected void func_230450_a_(MatrixStack p_230450_1_, float partialTicks, int mouseX, int mouseY)
     {
         RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
-        // TODO MCP-name: this.minecraft.getTextureManager().bindTexture(HOPPER_GUI_TEXTURE);
+        // TODO MCP-name: field_230706_i_ -> minecraft
         this.field_230706_i_.getTextureManager().bindTexture(HOPPER_GUI_TEXTURE);
-        // TODO MCP-name: int startX = (this.width - this.xSize) / 2;
+        // TODO MCP-name: field_230708_k_ -> width
         int startX = (this.field_230708_k_ - this.xSize) / 2;
-        // TODO MCP-name: int startY = (this.height - this.ySize) / 2;
+        // TODO MCP-name: field_230709_l_ -> height
         int startY = (this.field_230709_l_ - this.ySize) / 2;
-        // TODO MCP-name: this.blit(startX, startY, 0, 0, this.xSize, this.ySize);
+        // TODO MCP-name: func_238474_b_ -> blit
         this.func_238474_b_(p_230450_1_, startX, startY, 0, 0, this.xSize, this.ySize);
 
         Slot slot = this.container.getSlot(0);
         if(!slot.getHasStack())
         {
-            // TODO MCP-name: blit(this.guiLeft + slot.xPos, this.guiTop + slot.yPos, this.xSize, 0, 16, 16);
+            // TODO MCP-name: func_238474_b_ -> blit
             func_238474_b_(p_230450_1_, this.guiLeft + slot.xPos, this.guiTop + slot.yPos, this.xSize, 0, 16, 16);
         }
     }
