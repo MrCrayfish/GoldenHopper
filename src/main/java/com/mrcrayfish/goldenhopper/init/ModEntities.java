49c229c4e7f6cbcd2ce9ebe1fc01328f30aa6b57
Autovw <68228006+Autovw@users.noreply.github.com>
2021-12-04 19:07:54 +0100

    ⬆ Updated to Forge 1.18

--- src/main/java/com/mrcrayfish/goldenhopper/init/ModEntities.java
+++ src/main/java/com/mrcrayfish/goldenhopper/init/ModEntities.java
@@ -5,9 +5,9 @@ import com.mrcrayfish.goldenhopper.client.ClientHandler;
 import com.mrcrayfish.goldenhopper.world.entity.vehicle.GoldenHopperMinecart;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
-import net.minecraftforge.fmllegacy.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 /**
  * Author: MrCrayfish
--- src/main/java/com/mrcrayfish/goldenhopper/init/ModItems.java
+++ src/main/java/com/mrcrayfish/goldenhopper/init/ModItems.java
@@ -4,9 +4,9 @@ import com.mrcrayfish.goldenhopper.Reference;
 import com.mrcrayfish.goldenhopper.world.level.item.GoldenHopperMinecartItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
-import net.minecraftforge.fmllegacy.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 /**
  * Author: MrCrayfish
