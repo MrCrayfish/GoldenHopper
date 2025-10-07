4b79f7ce62be12d013320624e71c0016fb8d48c2
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2025-10-07 18:54:45 +1030

    📦 Tag golden hopper as piglin loved

--- common/src/main/java/com/mrcrayfish/goldenhopper/data/CommonItemTagsProvider.java
+++ common/src/main/java/com/mrcrayfish/goldenhopper/data/CommonItemTagsProvider.java
@@ -1,9 +1,11 @@
 package com.mrcrayfish.goldenhopper.data;
 
+import com.mrcrayfish.goldenhopper.core.ModBlocks;
 import com.mrcrayfish.goldenhopper.platform.Services;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.VanillaItemTagsProvider;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.world.item.Items;
 
 import java.util.concurrent.CompletableFuture;
@@ -22,5 +24,6 @@ public class CommonItemTagsProvider extends VanillaItemTagsProvider
     protected void addTags(HolderLookup.Provider provider)
     {
         this.tag(Services.PLATFORM.getGoldIngotsTag()).add(Items.GOLD_INGOT);
+        this.tag(ItemTags.PIGLIN_LOVED).add(ModBlocks.GOLDEN_HOPPER.get().asItem());
     }
 }
