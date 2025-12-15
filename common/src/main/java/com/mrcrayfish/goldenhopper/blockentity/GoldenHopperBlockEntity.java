ba0f8f02404f57b6d2d7a4f817790b93188e7ca5
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2025-12-15 14:43:50 +1030

    ✨ Potions and enchanted books can now be sorted

--- common/src/main/java/com/mrcrayfish/goldenhopper/blockentity/GoldenHopperBlockEntity.java
+++ common/src/main/java/com/mrcrayfish/goldenhopper/blockentity/GoldenHopperBlockEntity.java
@@ -4,18 +4,25 @@ import com.mrcrayfish.goldenhopper.core.ModBlockEntities;
 import com.mrcrayfish.goldenhopper.inventory.GoldenHopperMenu;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.core.Holder;
 import net.minecraft.core.NonNullList;
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.Container;
 import net.minecraft.world.WorldlyContainer;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.alchemy.Potion;
+import net.minecraft.world.item.alchemy.PotionContents;
+import net.minecraft.world.item.enchantment.ItemEnchantments;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.entity.HopperBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Objects;
+import java.util.Optional;
 import java.util.stream.IntStream;
 
 /**
@@ -72,7 +79,40 @@ public class GoldenHopperBlockEntity extends HopperBlockEntity implements Worldl
     @Override
     public boolean canPlaceItem(int index, ItemStack stack)
     {
-        return index != FILTER_SLOT_INDEX && (this.getItems().get(FILTER_SLOT_INDEX).isEmpty() || stack.getItem() == this.getItems().get(FILTER_SLOT_INDEX).getItem());
+        if(index != FILTER_SLOT_INDEX)
+        {
+            ItemStack filter = this.getItems().get(FILTER_SLOT_INDEX);
+            if(filter.isEmpty())
+                return true;
+
+            if(!stack.is(filter.getItem()))
+                return false;
+
+            // Potion contents must match if filter has potion contents
+            if(filter.has(DataComponents.POTION_CONTENTS))
+            {
+                if(!stack.has(DataComponents.POTION_CONTENTS))
+                    return false;
+
+                PotionContents first = filter.get(DataComponents.POTION_CONTENTS);
+                PotionContents second = stack.get(DataComponents.POTION_CONTENTS);
+                if(first != null && second != null && !comparePotions(first, second))
+                    return false;
+            }
+
+            // Item enchantments must match if filter has potion contents
+            if(filter.has(DataComponents.STORED_ENCHANTMENTS))
+            {
+                if(!stack.has(DataComponents.STORED_ENCHANTMENTS))
+                    return false;
+
+                ItemEnchantments first = filter.get(DataComponents.STORED_ENCHANTMENTS);
+                ItemEnchantments second = stack.get(DataComponents.STORED_ENCHANTMENTS);
+                if(first != null && second != null && !compareEnchantments(first, second))
+                    return false;
+            }
+        }
+        return true;
     }
 
     @Override
@@ -91,5 +131,44 @@ public class GoldenHopperBlockEntity extends HopperBlockEntity implements Worldl
     public boolean canTakeItem(Container container, int index, ItemStack stack)
     {
         return index != FILTER_SLOT_INDEX;
+    }
+
+    private static boolean comparePotions(PotionContents first, PotionContents second)
+    {
+        // Compare the potion value
+        Optional<Holder<Potion>> firstOptional = first.potion();
+        if(firstOptional.isPresent())
+        {
+            Optional<Holder<Potion>> secondOptional = second.potion();
+            if(secondOptional.isEmpty())
+                return false;
+
+            Potion firstPotion = firstOptional.get().value();
+            Potion secondPotion = secondOptional.get().value();
+            if(firstPotion != secondPotion)
+                return false;
+        }
+
+        // The matching stack must have at least the base effects of the filter.
+        for(var instance : first.customEffects())
+        {
+            if(second.customEffects().stream().noneMatch(effect -> effect.getEffect().equals(instance.getEffect())))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean compareEnchantments(ItemEnchantments first, ItemEnchantments second)
+    {
+        for(var holder : first.keySet())
+        {
+            if(second.getLevel(holder) <= 0)
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
